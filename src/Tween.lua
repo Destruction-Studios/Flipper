ce4e129c84a8d1607cf451eb8c471d3f966c0b66
Destruction Studios <founderofbeans@gmail.com>
2025-06-28 21:20:44 -0400

    fixed PascalCasing

--- src/Tween.lua
+++ src/Tween.lua
@@ -33,7 +33,7 @@ function Tween.new(targetValue: number, tweenInfo: TweenInfo?)
 	return setmetatable(tweenProperties, Tween)
 end
 
-function Tween:Step(state, delta)
+function Tween:step(state, delta)
 	local tween = self._Tween
 	local tweenObject = self._TweenObject
 	local playbackState = tween.PlaybackState
